b6b7e331d0bf5f9e8e919afd7352fc6913382f56
Hyewon Shin <posie0064@naver.com>
2024-03-04 06:01:02 +0900

    3rd 수정

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,14 @@ function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  fetch(url); //network > fetch에서 url결과확인가능
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp} °C`;
+      city.innerText = data.name;
+    }); //network > fetch에서 url결과확인가능
 }
 
 function onGeoError() {
